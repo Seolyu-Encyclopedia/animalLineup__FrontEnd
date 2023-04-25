2f5b60baa1b59866726ce314bf5bcc65c0812b06
seolyu_lee <seolyu_lee@tmax.co.kr>
2023-04-25 19:17:53 +0900

    project setting

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="logoImage"></div>
+      </header>
+      <main>
+        <div className="diary">다이어리</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
