9cc67cfc41fcc4e88f4bdae60f7de8f9a446a593
seolyu_lee <seolyu_lee@tmax.co.kr>
2023-04-28 16:09:22 +0900

    add mouse control to the cube using OrbitControls

--- src/components/ThreeDSpace.js
+++ src/components/ThreeDSpace.js
@@ -1,27 +1,46 @@
-import React, { useRef } from "react";
-import styles from "./ThreeDSpace.module.css";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { BoxGeometry, MeshStandardMaterial } from "three";
+import React, { useRef } from 'react';
+import styles from './ThreeDSpace.module.css';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { BoxGeometry, MeshStandardMaterial } from 'three';
+import { OrbitControls } from '@react-three/drei';
 
 function Rubik() {
   const ref = useRef();
   const speed = 0.01;
   const cubeSize = 3;
   const margin = 0.2;
+  const mouse = useRef([0, 0]);
+  const isMouseDown = useRef(false);
 
   useFrame(() => {
-    ref.current.rotation.x += speed;
-    ref.current.rotation.y += speed;
+    if (isMouseDown.current) {
+      const [x, y] = mouse.current;
+      ref.current.rotation.y += (x - mouse.current[0]) * 0.005;
+      ref.current.rotation.x += (y - mouse.current[1]) * 0.005;
+      mouse.current = [x, y];
+    } else {
+      ref.current.rotation.x += speed;
+      ref.current.rotation.y += speed;
+    }
   });
+
+  const handleMouseDown = (e) => {
+    isMouseDown.current = true;
+    mouse.current = [e.clientX, e.clientY];
+  };
+
+  const handleMouseUp = () => {
+    isMouseDown.current = false;
+  };
 
   const geometry = new BoxGeometry(cubeSize, cubeSize, cubeSize);
   const materials = [
-    new MeshStandardMaterial({ color: "blue" }),
-    new MeshStandardMaterial({ color: "green" }),
-    new MeshStandardMaterial({ color: "white" }),
-    new MeshStandardMaterial({ color: "yellow" }),
-    new MeshStandardMaterial({ color: "orange" }),
-    new MeshStandardMaterial({ color: "red" }),
+    new MeshStandardMaterial({ color: 'blue' }),
+    new MeshStandardMaterial({ color: 'green' }),
+    new MeshStandardMaterial({ color: 'white' }),
+    new MeshStandardMaterial({ color: 'yellow' }),
+    new MeshStandardMaterial({ color: 'orange' }),
+    new MeshStandardMaterial({ color: 'red' }),
   ];
 
   const cubes = [];
@@ -38,6 +57,8 @@ function Rubik() {
               j * (cubeSize + margin),
               k * (cubeSize + margin),
             ]}
+            onPointerDown={handleMouseDown}
+            onPointerUp={handleMouseUp}
           />
         );
         cubes.push(mesh);
@@ -63,6 +84,7 @@ function ThreeDSpace() {
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Rubik />
+        <OrbitControls />
       </Canvas>
     </div>
   );
